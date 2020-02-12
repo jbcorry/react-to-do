f891f6137edfe421cf86ad7f7c7f0fdb2d860c37
Jacob Corry <jcorry@SLCM-JCORRY.mychg.com>
2020-02-12 14:03:57 -0700

    dynamic data using axios

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Rainbow from '../hoc/Rainbow'
+import axios from 'axios'
 
-const About = () => {
-    return (
-        <div className="container">
-        <h4 className="center">About</h4>
-        <p>ipsum
-        </p>
-        </div>
-    )
+class About extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    posts: res.data.slice(0,10)
+                })
+            })
+    }
+    render () {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
+        return (
+            <div className="container">
+                <h4 className="center">About</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Rainbow(About);
